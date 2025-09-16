f7be2b10044c4f12731c221b006cdd9f967643a2
Ricards <a240706rl@rvt.lv>
2025-09-16 04:34:06 -0700

    average of three numbers

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("Ievadiet pirmo skaitli:`\n");
+        System.out.printf("Ievadiet pirmo skaitli:\n");
         int first = Integer.valueOf(scanner.nextLine());
         System.out.printf("Ievadiet otro skaitli:\n");
         int second = Integer.valueOf(scanner.nextLine());
-        System.out.printf("%d + %d = %d",first, second, first+second);
+        System.out.printf("Ievadiet treso skaitli:\n");
+        int third = Integer.valueOf(scanner.nextLine());
+        double sum = (double) (first+second+third) / 3;
+        System.out.printf("%.1f", sum);
     }
 }
