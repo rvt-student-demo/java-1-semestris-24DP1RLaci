b9203bb3eec19c5d69f7b4eb016fd7c7f5db93e8
Ricards <a240706rl@rvt.lv>
2025-09-16 04:11:09 -0700

    addition formula

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,13 @@
 package rvt;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.printf("Hello world?");
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Ievadiet pirmo skaitli:`\n");
+        int first = Integer.valueOf(scanner.nextLine());
+        System.out.printf("Ievadiet otro skaitli:\n");
+        int second = Integer.valueOf(scanner.nextLine());
+        System.out.printf("%d + %d = %d",first, second, first+second);
     }
 }
