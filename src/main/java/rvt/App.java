e49635120df3a7b541a3eb31528e41c90112d6f3
Ricards <a240706rl@rvt.lv>
2025-09-18 02:24:41 -0700

    Odd or even

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -4,24 +4,12 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("Ievadiet punktus:\n");
-        int punkti = Integer.valueOf(scanner.nextLine());
-        if (punkti < 0) {
-            System.out.printf("Nav iespejams");
-        } else if (punkti > 0 && punkti <= 49) {
-            System.out.printf("izgāzās");
-        } else if (punkti >= 50 && punkti <= 59 ) {
-            System.out.printf("1");
-        } else if (punkti >= 60 && punkti <= 69 ) {
-            System.out.printf("2");
-        } else if (punkti >= 70 && punkti <= 79 ) {
-            System.out.printf("3");
-        } else if (punkti >= 80 && punkti <= 89 ) {
-            System.out.printf("4");
-        } else if (punkti >= 90 && punkti <= 100 ) {
-            System.out.printf("5");
-        } else if (punkti > 100) {
-            System.out.printf("Lieliski");
+        System.out.printf("Give a number:\n");
+        int number = Integer.valueOf(scanner.nextLine());
+        if (number % 2 == 0) {
+            System.out.printf("Number %d is even", number);
+        } else {
+            System.out.printf("Number %d is odd", number);
         }
     }
 }
