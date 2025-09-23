c7f335747e3f93484b52251938fbc04aa5883094
Ricards <a240706rl@rvt.lv>
2025-09-23 04:13:15 -0700

    Detaļas

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -3,13 +3,28 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        final int skruvjucena = 5;
+        final int uzgrievnucena = 3;
+        final int paplaknsescena = 1;
+        
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("Give a number:\n");
-        int number = Integer.valueOf(scanner.nextLine());
-        if (number % 2 == 0) {
-            System.out.printf("Number %d is even", number);
-        } else {
-            System.out.printf("Number %d is odd", number);
+        System.out.printf("Skrūvju skaits: ");
+        int skruves = Integer.valueOf(scanner.nextLine());
+        System.out.printf("Uzgriežņu skaits: ");
+        int uzgriezni = Integer.valueOf(scanner.nextLine());
+        System.out.printf("Paplākšņu skaits: ");
+        int paplaksnes = Integer.valueOf(scanner.nextLine());
+        scanner.close();
+        
+        if (uzgriezni >= skruves && skruves <= paplaksnes / 2) {
+            System.out.printf("Pasūtījums ir kārtībā\n");
         }
+        if (uzgriezni < skruves) {
+            System.out.printf("Pābaudi pasūtījumu: par maz uzgriežņu\n");
+        }
+        if (skruves > paplaksnes / 2) {
+            System.out.printf("Pārbaudi pasūtījumu: par maz paplākšņu\n");
+        }
+        System.out.printf("Kopējā cena: %d", (skruves * skruvjucena) + (uzgriezni * uzgrievnucena) + (paplaksnes * paplaknsescena));
     }
 }
