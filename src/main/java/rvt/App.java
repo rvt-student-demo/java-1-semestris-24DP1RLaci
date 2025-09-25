c9c0d605561db7b4e6b2b4aba61d34c6b561c18b
Ricards <a240706rl@rvt.lv>
2025-09-25 03:20:18 -0700

    numbers and sum of numbers

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -3,26 +3,22 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        final int skruvjucena = 5;
-        final int uzgrievnucena = 3;
-        final int paplaknsescena = 1;
-
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("Ievadi preci: ");
-        String prece = String.valueOf(scanner.nextLine());
-        System.out.printf("Ievadi cenu: ");
-        Double cena = Double.valueOf(scanner.nextLine());
-        System.out.printf("Ekspress piegāde (0 = nē, 1 = jā): ");
-        int piegade = Integer.valueOf(scanner.nextLine());
-        scanner.close();
-        System.out.printf("Rēķins:\n");
-        System.out.printf("     %s %10.2f\n", prece, cena);
-        if (cena < 10.0) {
-            System.out.printf("     Piegāde: %10.2f\n", 2.0 + piegade * 3.0);
-            System.out.printf("     Kopā: %10.2f\n", cena + 2.0 + (piegade * 3.0));
-        } else {
-            System.out.printf("     Piegāde: %10.2f\n", piegade * 3.0);
-            System.out.printf("     Kopā: %10.2f\n", cena + (piegade * 3));
-        }
+        int loops = 0;
+        int sum = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 0) {
+                break;
+            } else {
+                loops += 1;
+                sum += input;
+            }
+
+        }   
+        System.out.println("Number of numbers: " + loops);
+        System.out.println("Sum of the numbers: " + sum);
     }
+    
 }
