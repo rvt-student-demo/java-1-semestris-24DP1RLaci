9c2daf8c887ac0c361e672a793d26549b5d2f40c
Xc987 <ricards.lacis18@gmail.com>
2025-10-03 10:45:31 +0300

    Advanced astrology (part 1)

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -3,14 +3,18 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        divisablebythreeinrange(2, 10);
         
     }
-    public static void divisablebythreeinrange(Integer start, Integer end) {
-        for (int i = start; i  <= end; i++) {
-            if (i % 3 == 0) {
-                System.out.printf("%d\n", i);
-            }
+    public static void printSpaces(int number) {
+        for (int i = 0; i < number; i ++) {
+            System.out.printf(" ");
         }
+        System.out.printf("\n");
+    }
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i ++) {
+            System.out.printf("*");
+        }
+        System.out.printf("\n");
     }
 }
