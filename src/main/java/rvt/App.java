5fa6796cf53851941fc179a9f18f5066e5e1e3e3
Xc987 <ricards.lacis18@gmail.com>
2025-10-03 10:50:46 +0300

    Advanced astrology (part 2)

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -3,18 +3,23 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        
+        printTriangle(4);
     }
     public static void printSpaces(int number) {
         for (int i = 0; i < number; i ++) {
             System.out.printf(" ");
         }
-        System.out.printf("\n");
     }
     public static void printStars(int number) {
         for (int i = 0; i < number; i ++) {
             System.out.printf("*");
         }
-        System.out.printf("\n");
+    }
+    public static void printTriangle(int size) {
+        for (int i = 0; i < size; i ++) {
+            printSpaces(size - (i+ 1));
+            printStars(i + 1);
+            System.out.printf("\n");
+        }
     }
 }
