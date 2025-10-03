cc695d81efd4cf56f46a27068e35c681ae174e6f
Xc987 <ricards.lacis18@gmail.com>
2025-10-03 10:59:17 +0300

    Advanced astrology (part 3)

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        printTriangle(4);
+        christmasTree(10);
     }
     public static void printSpaces(int number) {
         for (int i = 0; i < number; i ++) {
@@ -19,6 +19,18 @@ public class App {
         for (int i = 0; i < size; i ++) {
             printSpaces(size - (i+ 1));
             printStars(i + 1);
+            System.out.printf("\n");
+        }
+    }
+    public static void christmasTree(int height) {
+        for (int i = 0; i < height; i ++) {
+            printSpaces(height - (i + 1));
+            printStars(i + 1 + i*1);
+            System.out.printf("\n");
+        }
+        for (int i = 0; i < 2; i ++) {
+            printSpaces(height - 2);
+            printStars(3);
             System.out.printf("\n");
         }
     }
