9927ccaee6ce9ac24c7f5cdb22d8eef0c43807f0
Ricards <a240706rl@rvt.lv>
2025-10-07 03:33:23 -0700

    Print in stars

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        int[] numbers = {5, 1, 3, 4, 2};
-        sumOfNumbersInArray(numbers);
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayInStars(array);
     }
-    public static int sumOfNumbersInArray(int[] array) {
-        int sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            sum += array[i];
+    public static void printArrayInStars(int[] array) {
+        for (int i = 0; i < array.length; i++){
+            for (int j = 0; j < array[i]; j++) {
+                System.out.printf("*");
+            }
+            System.out.printf("\n");
         }
-        System.out.printf("%d", sum);
-        return sum;
     }
 }
