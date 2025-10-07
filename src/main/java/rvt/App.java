7a5ccd0209519d48f9e70c5c0deaa1d9f47c5d47
Ricards <a240706rl@rvt.lv>
2025-10-07 03:26:37 -0700

    swap

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -3,35 +3,20 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        christmasTree(10);
-    }
-    public static void printSpaces(int number) {
-        for (int i = 0; i < number; i ++) {
-            System.out.printf(" ");
+        Scanner reader = new Scanner(System.in);
+        int[] numbers = {4,2,9,5,1,6};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.printf("%d\n", numbers[i]);
         }
-    }
-    public static void printStars(int number) {
-        for (int i = 0; i < number; i ++) {
-            System.out.printf("*");
-        }
-    }
-    public static void printTriangle(int size) {
-        for (int i = 0; i < size; i ++) {
-            printSpaces(size - (i+ 1));
-            printStars(i + 1);
-            System.out.printf("\n");
-        }
-    }
-    public static void christmasTree(int height) {
-        for (int i = 0; i < height; i ++) {
-            printSpaces(height - (i + 1));
-            printStars(i + 1 + i*1);
-            System.out.printf("\n");
-        }
-        for (int i = 0; i < 2; i ++) {
-            printSpaces(height - 2);
-            printStars(3);
-            System.out.printf("\n");
+        System.out.printf("\nGive two indices to swap:\n");
+        int pirm = Integer.valueOf(reader.nextLine());
+        int otrs = Integer.valueOf(reader.nextLine());
+        int pirmback = numbers[pirm];
+        numbers[pirm] = numbers[otrs];
+        numbers[otrs] = pirmback;
+        System.out.printf("\n");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.printf("%d\n", numbers[i]);
         }
     }
 }
