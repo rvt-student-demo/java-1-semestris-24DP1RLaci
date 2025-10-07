302566d86a221a9d99117a59192eaec7fe878c52
Ricards <a240706rl@rvt.lv>
2025-10-07 03:29:35 -0700

    Sum of array

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -4,19 +4,15 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        int[] numbers = {4,2,9,5,1,6};
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.printf("%d\n", numbers[i]);
+        int[] numbers = {5, 1, 3, 4, 2};
+        sumOfNumbersInArray(numbers);
+    }
+    public static int sumOfNumbersInArray(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
         }
-        System.out.printf("\nGive two indices to swap:\n");
-        int pirm = Integer.valueOf(reader.nextLine());
-        int otrs = Integer.valueOf(reader.nextLine());
-        int pirmback = numbers[pirm];
-        numbers[pirm] = numbers[otrs];
-        numbers[otrs] = pirmback;
-        System.out.printf("\n");
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.printf("%d\n", numbers[i]);
-        }
+        System.out.printf("%d", sum);
+        return sum;
     }
 }
