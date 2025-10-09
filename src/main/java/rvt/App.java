972065b05f46b76007b298edf21e3825eaeba1dc
Ricards <a240706rl@rvt.lv>
2025-10-09 02:22:41 -0700

    chapter 60 uzdevumi

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -4,15 +4,38 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        int[] array = {5, 1, 3, 4, 2};
-        printArrayInStars(array);
+        ex1();
+        ex2();
+        ex3();
     }
-    public static void printArrayInStars(int[] array) {
-        for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < array[i]; j++) {
-                System.out.printf("*");
-            }
-            System.out.printf("\n");
+    public static void ex1() {
+        int[] val = {0, 1, 2, 3}; 
+        int sum = 0;
+        for (int i = 0; i < val.length; i++) {
+            sum += val[i];
         }
+ 
+        System.out.println( "Sum of all numbers = " + sum );
+    }
+    public static void ex2() {
+        int[] val = {13, -4, 82, 17}; 
+        int[] twice =  new int[val.length];
+        
+        System.out.println( "Original Array: " + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+        for (int i = 0; i < val.length; i++) {
+            twice[i] = val[i] * 2;
+        }
+    
+        System.out.println( "New Array: " + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+    }
+    public static void ex3() {
+        int[] valA   = { 13, -22,  82,  17}; 
+        int[] valB   = {-12,  24, -79, -13};
+        int[] sum    = {  0,   0,   0,   0};
+        for (int i = 0; i < valA.length; i++) {
+            sum[i] = valA[i] + valB[i];
+        }
+ 
+        System.out.println( "sum: " + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
     }
 }
