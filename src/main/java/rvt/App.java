1fa8417948a663a779d8bf0a1bc9a782b3da582e
Ricards <a240706rl@rvt.lv>
2025-11-25 23:52:23 -0800

    books

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,29 +1,50 @@
 package rvt;
-import java.util.Scanner;
 
+import java.util.Scanner;
+import java.util.ArrayList;
 public class App {
+    String name;
+    Integer pages;
+    Integer releaseYear;
+
+    public App(String name, Integer pages, Integer releaseYear) {
+        this.name = name;
+        this.pages = pages;
+        this.releaseYear = releaseYear;
+    }
+    public void printinfo() {
+        System.out.printf("%s, %s, %s", name, pages, releaseYear);
+    }
+    public void printname() {
+        System.out.printf("%s", name);
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Statistics statistics = new Statistics();
-        Statistics statisticsEven = new Statistics();
-        Statistics statisticsOdd = new Statistics();
+        ArrayList<App> books = new ArrayList<>();
         while (true) {
-            Integer skaitlis = Integer.valueOf(scanner.nextLine());
-            if (skaitlis != -1) {
-                if (skaitlis % 2 == 0) {
-                    statisticsEven.addNumber(skaitlis);
-                } else {
-                    statisticsOdd.addNumber(skaitlis);
-                }
-                statistics.addNumber(skaitlis);
+            System.out.printf("Title: ");
+            String name = String.valueOf(scanner.nextLine());
+            if (!(name.equals(""))) {
+                System.out.printf("Pages: ");
+                Integer pages = Integer.valueOf(scanner.nextLine());
+                System.out.printf("Publication year: ");
+                Integer year = Integer.valueOf(scanner.nextLine());
+                books.add(new App(name, pages, year));
             } else {
                 break;
             }
         }
-        // System.out.println("Count: " + statistics.getCount());
-        System.out.println("Sum: " + statistics.sum());
-        System.out.println("Sum of even numbers: " + statisticsEven.sum());
-        System.out.println("Sum of odd numbers: " + statisticsOdd.sum());
-        // System.out.println("Average: " + statistics.average());
+        System.out.printf("\nWhat information will be printed? ");
+        String answer = String.valueOf(scanner.nextLine());
+        if (answer.equals("everything")) {
+            for (App b : books) {
+                b.printinfo();
+            }
+        } else if (answer.equals("name")) {
+            for (App b : books) {
+                b.printname();
+            }
+        }
+        
     }
 }
