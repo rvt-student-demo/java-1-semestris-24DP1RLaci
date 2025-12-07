caae0256bc8803857d893c901ea41c79c11f5ace
Xc987 <ricards.lacis18@gmail.com>
2025-12-07 16:28:30 +0200

    payment card

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,28 +1,33 @@
 package rvt;
 
 import java.util.Scanner;
-import java.util.Random;
 
 public class App {
     int number;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        int cipars = random.nextInt(6) + 1;
-        Boolean uzvareja = false;
-        System.out.printf("Es domāju par skaitli no 1 līdz 10.\nTev tas jāuzmin trīs mēģinājumos.\nIevadi minējumu:\n");
-        for (int i = 0; i < 3; i ++) {
-            int value = Integer.valueOf(scanner.nextLine());
-            if (cipars == value) {
-                System.out.printf("Pareizi!\nTu esi uzvarējis spēli\n");
-                uzvareja = true;
-                break;
-            } else {
-                System.out.printf("Nepareizi\n");
-            }
-        }
-        if (!uzvareja) {
-            System.out.printf("Pareizais skaitlis bija %d.\nTu esi zaudējis spēli.\n", cipars);
-        }
+
+        PaymentCard paul = new PaymentCard(20);
+        PaymentCard matt = new PaymentCard(30);
+
+        paul.eatHeartily();
+        matt.eatAffordably();
+
+        System.out.println("Paul: " + paul.toString());
+        System.out.println("Matt: " + matt.toString());
+
+        paul.addMoney(20);
+        matt.eatHeartily();
+
+        System.out.println("Paul: " + paul.toString());
+        System.out.println("Matt: " + matt.toString());
+
+        paul.eatAffordably();
+        paul.eatAffordably();
+
+        matt.addMoney(50);
+
+        System.out.println("Paul: " + paul.toString());
+        System.out.println("Matt: " + matt.toString());
     }
 }
